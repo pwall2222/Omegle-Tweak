16785e78481c7672f9ede7a02f8993c618b89bc9
PWall <34860495+PWall2222@users.noreply.github.com>
2021-02-06 08:59:40 +0100

    Added more coperabiltie with the page source

--- dom.js
+++ dom.js
@@ -1,3 +1,29 @@
+function init_chat() {
+	document.getElementsByClassName("logitem")[0].innerHTML = /* html */`
+	IP: Fetching <br/>
+	City: Fetching <br/>
+	Region: Fetching <br/>
+	Country: Fetching <br/>
+	ISP: Fetching <br/>
+	<div id="time">Time: Fetching </div>
+	Timezone: Fetching <br/>
+	<div id="ctime">Connection: 00:00:00</div>
+	<button type="button" onclick="list.blacklist_this()">Blacklist this IP</button>
+	<button type="button" onclick="list.whitelist_this()">Whitelist this IP</button> <br/>
+	<button type="button" onclick="dclick.new_connection()">New Connection</button>
+	<button type="button" onclick="dclick.discconect()">Skip</button>
+	<button type="button" onclick="dclick.reroll_skip()">Skip and stop reroll</button> <br/>
+	<button type="button" onclick="local.save()">Save to local storage</button>
+	<button type="button" onclick="local.clear()">Clear local storage</button>`;
+}
+const reg = /\("Omegle video chat might not work well in Microsoft Edge\. Please upgrade to Firefox or Chrome\."\)/g
+const string = `("Omegle video chat might not work well in Microsoft Edge. Please upgrade to Firefox or Chrome."),init_chat()`;
+let chat = startNewChat.toString();
+chat = chat.substring(35);
+chat = chat.substring(0, chat.length - 1);
+chat = chat.replace(reg, string);
+startNewChat = new Function (["a","b","c","e","f"], chat);
+setShouldUseEnglish(true);
 const newItem = document.createElement("div");
 newItem.innerHTML = /* html */`
 	<div style="width: 400px;height: 20px;margin-top: 7px;margin-right: auto;margin-bottom: auto;margin-left: auto;"> 
@@ -37,30 +63,3 @@ $("onlinecount").style.bottom = "18px";
 $('sharebuttons').dispose();
 $('tagline').dispose();
 $('footer').dispose();
-const observer = new MutationObserver(obscallback);
-function obscallback(mutations) {
-	if (mutations.some(mutation => 
-		mutation.addedNodes.length && 
-		mutation.addedNodes[0].innerText != undefined && 
-		mutation.addedNodes[0].innerText.includes("You're now chatting with a random stranger.")
-	))
-	{
-		document.getElementsByClassName("logitem")[0].innerHTML = /* html */`
-		IP: Fetching <br/>
-		City: Fetching <br/>
-		Region: Fetching <br/>
-		Country: Fetching <br/>
-		ISP: Fetching <br/>
-		<div id="time">Time: Fetching </div>
-		Timezone: Fetching <br/>
-		<div id="ctime">Connection: 00:00:00</div>
-		<button type="button" onclick="list.blacklist_this()">Blacklist this IP</button>
-		<button type="button" onclick="list.whitelist_this()">Whitelist this IP</button> <br/>
-		<button type="button" onclick="dclick.new_connection()">New Connection</button>
-		<button type="button" onclick="dclick.discconect()">Skip</button>
-		<button type="button" onclick="dclick.reroll_skip()">Skip and stop reroll</button> <br/>
-		<button type="button" onclick="local.save()">Save to local storage</button>
-		<button type="button" onclick="local.clear()">Clear local storage</button>`;
-	}
-}
-observer.observe(document, {childList: true, subtree: true});
