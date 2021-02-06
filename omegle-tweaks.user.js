a7d9119ee60d980d9147fd14a5bda1291ee622a1
PWall <34860495+PWall2222@users.noreply.github.com>
2021-02-06 09:01:09 +0100

    Fixed user script

--- omegle-tweaks.user.js
+++ omegle-tweaks.user.js
@@ -1,18 +1,18 @@
 // ==UserScript==
-// @name         Omegle
+// @name         Omegle-Tweaks
 // @version      1.0
 // @author       PWall
 // @include      https://omegle.com/*
 // @include      https://www.omegle.com/*
-// @run-at       document-start
+// @run-at       document-end
 // @grant        none
 // ==/UserScript==
 
 (function() {
 	const dom = document.createElement('script');
-	dom.src = 'https://raw.githubusercontent.com/PWalll/omegle-tweak/master/dom.js';
+	dom.src = 'https://cdn.jsdelivr.net/gh/PWalll/omegle-tweak@0.1/dom.js';
 	document.head.appendChild(dom);
 	const index = document.createElement('script');
-	index.src = 'https://raw.githubusercontent.com/PWalll/omegle-tweak/master/index.js';
+	index.src = 'https://cdn.jsdelivr.net/gh/PWalll/omegle-tweak@0.1/index.js';
 	document.head.appendChild(index);
 })();
